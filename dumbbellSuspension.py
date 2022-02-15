055c9c6c7982cec7d3baf11294cdee6bc00d5a9e
subhamkd <subhamkdas98@gmail.com>
2022-02-14 17:06:37 -0700

    added new script for dumbbell suspension

---
+++ dumbbellSuspension.py
@@ -0,0 +1,28 @@
+'''
+
+Author : Subham Das
+University of Calgary
+
+'''
+
+import numpy as np
+from scipy.optimize import fsolve
+from scipy.integrate import odeint
+
+class suspension:
+	"""docstring for suspension"""
+	def __init__(self, De, E, Ld, b):
+		#super(suspension, self).__init__()
+		self.De = De		# Deborah number
+		self.E = E			# Charge density parameter
+		self.Ld = Ld		# dimensionless Debye length
+		self.b = b			# finite extensibility parameter
+		QQ_eq = 
+
+
+	def shear(s,t=0): #apply a simple x-direction shear with shear rate s
+
+
+
+
+	def extension(e,t=0): #
