986dee282f472807c9493a27de4c4f2a788a592a
subhamkd <subhamkdas98@gmail.com>
2022-03-26 18:49:55 -0600

    added polymeric stress calculations

--- dumbbellSuspension.py
+++ dumbbellSuspension.py
@@ -6,6 +6,7 @@ University of Calgary
 '''
 
 from scipy.optimize import fsolve
+import numpy as np
 
 class suspension: #creates a suspension of FENE dumbbells with relaxation time t_rel and finite extensibility parameter b
 
@@ -20,10 +21,24 @@ class suspension: #creates a suspension of FENE dumbbells with relaxation time t
 		print(self.QQ_eq)
 
 	def shear(self,s): 					#applies a steady shear with shear rate 's'
-		self.QQ = solve_QQ_eq(eq_steady,self.b,t_rel,s,0)
+		self.QQ = solve_QQ_eq(eq_steady,self.b, self.t_rel,s,0)
 	
 	def extension(self,e): 				#applies a steady elongation with elongation rate 'e'
-		self.QQ = solve_QQ_eq(eq_steady,self.b,t_rel,0,e)
+		self.QQ = solve_QQ_eq(eq_steady,self.b, self.t_rel,0,e)
+	
+	def polyStress(self):
+		b=self.b
+		QQ=np.array(self.QQ)
+		trQ=QQ[0]+QQ[3]+QQ[5]
+		tp=np.zeros(6)
+		tp[0]=(QQ[0]/(1-trQ/b)-1)
+		tp[1]=(QQ[1]/(1-trQ/b))
+		tp[2]=(QQ[2]/(1-trQ/b))
+		tp[3]=(QQ[3]/(1-trQ/b)-1)
+		tp[4]=(QQ[4]/(1-trQ/b))
+		tp[5]=(QQ[5]/(1-trQ/b)-1)
+		return tp
+	
 
 
 def eq_steady(vars, b, t_rel=1, s=0, e=0):
@@ -39,5 +54,5 @@ def eq_steady(vars, b, t_rel=1, s=0, e=0):
 
 def solve_QQ_eq(eq, b, t_rel=1, s=0, e=0):
 	params=(b,t_rel,s,e)
-	[Qxx,Qxy,Qxz,Qyy,Qyz,Qzz]=fsolve(eq_steady,(1,0,0,1,0,1),args=params)
+	[Qxx,Qxy,Qxz,Qyy,Qyz,Qzz]=fsolve(eq,(1,0,0,1,0,1),args=params)
 	return Qxx,Qxy,Qxz,Qyy,Qyz,Qzz
